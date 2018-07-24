9aeb1d09d5d57d9de08627b96153dfbb1164d146
Jae Seong <qkrwotjd1445@naver.com>
2018-07-24 06:48:25 +0000

    1

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 class NotesController < ApplicationController
     # before_action :set_note, only: [:show, :edit, :update, :destroy]
-    # before_action :authenticate_user!, except: [:stduy]
+    # before_action :authenticate_user!, except: [:study]
     
     def new
     end
