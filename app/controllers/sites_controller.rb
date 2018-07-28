7c475d5ab2aedef5ae9a1ba86fb5474d47c6a7c5
Jae Seong <qkrwotjd1445@naver.com>
2018-07-28 16:49:38 +0000

    fix menu button in header

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,23 @@
 class SitesController < ApplicationController
+    before_action :lnb_class1, only: [:study]
+    before_action :lnb_class2, only: [:competition]
+    before_action :lnb_class3, only: [:club]
+    
+    
+    def lnb_class1
+        @study = 'bg-color1 black3'
+    end
+    
+    def lnb_class2
+        @comp = 'bg-color1 black3'
+    end
+    
+    def lnb_class3
+        @club = 'bg-color1 black3'
+    end
+    
+    
+    
     def index
     end
     
